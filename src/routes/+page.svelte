6853e20a42d9b7ab0b42ea53037830a8ce34745d
nap-ste <st-dev-2@outlook.com>
2024-12-06 11:00:33 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+    let isDark = $state(false)
+    const switchTheme = () => {
+        if(isDark){
+            document.documentElement.setAttribute('data-theme', 'light')
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark')
+        }
+        isDark = !isDark
+    }
+
+    onMount(() => {
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+        if(prefersDarkMode.matches){
+            isDark = true
+        }
+    })
+</script>
+
+<button class="bg-white text-black" onclick={switchTheme}>switch theme</button>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
